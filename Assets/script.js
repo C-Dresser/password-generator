6c08adc0ba1de6064cd1779486739078723a869c
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-07 20:46:03 -0500

    added variables to js

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Housekeeping
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppdercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "1234567890"
+var special = "!@#$%^&*()?.,"
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
