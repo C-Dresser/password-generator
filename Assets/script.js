957946ae4de496c862b76db91c1b78fd386749e3
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-07 21:35:57 -0500

    added vars in Housekeeping comtaining the required password input criteria and booleans

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Housekeeping
-var lowercase = "abcdefghijklmnopqrstuvwxyz"
-var uppdercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "1234567890"
-var special = "!@#$%^&*()?.,"
-
 //wrote function to test button functionality
 function generatePassword() {
-  return "password"
+
+  var password = ""
+  var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  var uppdercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numbers = "1234567890"
+  var special = "!@#$%^&*()?.,"
+  var pwlength = 12
+  var hasLowerCase = false
+  var hasUpperCase = false
+  var hasNumbers = false
+  var hasSpecial = false
+
+  for (var i = 0; i < pwlength; i++)
+    {
+      var random = Math.floor(Math.random() * lowercase.length);
+      password += lowercase[random];
+    }
+  
+  return password
 }
 
 // Write password to the #password input
