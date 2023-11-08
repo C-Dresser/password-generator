f873dc93b50de3a8019d2ea03156f4a21ab68470
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-07 21:47:18 -0500

    added alert if no inputs are chosen

--- Assets/script.js
+++ Assets/script.js
@@ -3,22 +3,28 @@ var generateBtn = document.querySelector("#generate");
 
 //wrote function to test button functionality
 function generatePassword() {
-
+//Housekeeping
   var password = ""
+  var possible = ""
   var lowercase = "abcdefghijklmnopqrstuvwxyz"
   var uppdercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var numbers = "1234567890"
   var special = "!@#$%^&*()?.,"
   var pwlength = 12
-  var hasLowerCase = false
+  var hasLowerCase = true
   var hasUpperCase = false
   var hasNumbers = false
   var hasSpecial = false
 
+  if (!hasLowerCase && !hasUpperCase) {
+    alert("Please choose at least one character type!");
+    return;
+  }
+//added for loop to generate lowercase password
   for (var i = 0; i < pwlength; i++)
     {
-      var random = Math.floor(Math.random() * lowercase.length);
-      password += lowercase[random];
+      var random = Math.floor(Math.random() * possible.length);
+      password += possible[random];
     }
   
   return password
